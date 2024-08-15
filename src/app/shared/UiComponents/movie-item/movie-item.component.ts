dbc9fcb46b61b884b1bcf3380431ee12af10606f
maiallam57 <maimallam57@gmail.com>
2024-08-15 15:28:39 +0300

    mainComponents

---
+++ src/app/shared/UiComponents/movie-item/movie-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './movie-item.component.html',
+  styleUrl: './movie-item.component.css'
+})
+export class MovieItemComponent {
+
+}
